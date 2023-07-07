42d0ef862cb2821d64b4273dca1e35d3af95768d
kihanya11 <kihanya01@gmail.com>
2023-07-07 15:05:13 +0300

    wrong css file removed

--- app/Views/admin_dashboard.php
+++ app/Views/admin_dashboard.php
@@ -5,10 +5,10 @@
  
 </head>
 <body>
-<?php
+    <?php
       $uri = service('uri');
      ?>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
       <a class="navbar-brand" href="/">SafariStay</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
