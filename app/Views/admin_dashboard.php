441d1aaa0d809c6334d628a892204b4613211bc9
kihanya11 <kihanya01@gmail.com>
2023-07-07 13:50:13 +0300

    admin login and navbar added

--- app/Views/admin_dashboard.php
+++ app/Views/admin_dashboard.php
@@ -8,6 +8,7 @@
 <?php
       $uri = service('uri');
      ?>
+     
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
       <a class="navbar-brand" href="/">SafariStay</a>
