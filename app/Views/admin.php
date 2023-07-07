d7820120a581055efb1f188e68fc76020e898af6
kihanya11 <kihanya01@gmail.com>
2023-07-07 17:05:05 +0300

    admin lte added to both admin pages

--- app/Views/admin.php
+++ app/Views/admin.php
@@ -7,40 +7,9 @@
 </head>
 
 <body>
-<?php
-      $uri = service('uri');
-     ?>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-      <a class="navbar-brand" href="/">SafariStay</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <?php if (session()->get('isLoggedIn')): ?>
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item <?= ($uri->getSegment(1) == 'dashboard' ? 'active' : null) ?>">
-            <a class="nav-link"  href="/admin_dashboard">Dashboard</a>
-          </li>
-          <li class="nav-item <?= ($uri->getSegment(1) == 'profile' ? 'active' : null) ?>">
-            <a class="nav-link" href="/admin_profile">Profile</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav my-2 my-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="/logout">Logout</a>
-          </li>
-        </ul>
-      <?php else: ?>
-        <ul class="navbar-nav mr-auto">  
-          <li class="nav-item <?= ($uri->getSegment(1) == 'admin' ? 'active' : null) ?>">
-            <a class="nav-link" href="/admin-login">Admin Login</a>
-          </li>
-        </ul>
-        <?php endif; ?>
-      </div>
-      </div>
-    </nav>
+<?= $this->extend('admin_layout') ?>
+<?= $this->section('content') ?>
+
     <div class="container">
         <h2>Admin Notifications</h2>
         <?php if (session()->has('success')): ?>
@@ -97,6 +66,8 @@
 
         <a href="<?php echo base_url('admin_dashboard'); ?>" class="btn btn-primary">Back to Admin Dashboard</a>
     </div>
+    <?= $this->endSection() ?>
+
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
--- app/Views/admin_dashboard.php
+++ app/Views/admin_dashboard.php
@@ -9,92 +9,13 @@
     <!-- AdminLTE CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/css/adminlte.min.css">
 </head>
-<body class="hold-transition sidebar-mini">
-    <div class="wrapper">
+<body>
+    <?= $this->extend('admin_layout') ?>
+    <?= $this->section('content') ?>
 
-        <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <!-- Brand Logo -->
-            <a href="#" class="brand-link">
-                <span class="brand-text font-weight-light">SafariStay</span>
-            </a>
-
-            <!-- Sidebar -->
-            <div class="sidebar">
-                <!-- Sidebar user panel (optional) -->
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <!--    
-                <div class="image">
-                        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-                    </div>
-                    <div class="info">
-                -->
-                        <a href="#" class="d-block"><?= session()->get('admin_name') ?></a>
-                    </div>
-                </div>
-
-                <!-- Sidebar Menu -->
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
-                        <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
-                        <li class="nav-item has-treeview menu-open">
-                            <a href="#" class="nav-link active">
-                                <i class="nav-icon fas fa-tachometer-alt"></i>
-                                <p>
-                                    Dashboard
-                                    <i class="right fas fa-angle-left"></i>
-                                </p>
-                            </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="#" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Users</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="#" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Vendors</p>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="nav-icon fas fa-th"></i>
-                                <p>
-                                    Simple Link
-                                    <span class="right badge badge-danger">New</span>
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="<?= base_url('admin') ?>" class="nav-link">
-                                <i class="nav-icon fas fa-bell"></i>
-                                <p>
-                                    Notifications
-                                    <span class="right badge badge-primary"><?= $notificationCount ?></span>
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/logout" class="nav-link">
-                                <i class="nav-icon fas fa-sign-out-alt"></i>
-                                <p>
-                                    Logout
-                                </p>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <!-- /.sidebar-menu -->
-            </div>
-            <!-- /.sidebar -->
-        </aside>
 
         <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
+        <div class="container">
             <!-- Main content -->
             <section class="content">
                 <div class="container-fluid">
@@ -109,11 +30,9 @@
             </section>
             <!-- /.content -->
         </div>
-        <!-- /.content-wrapper -->
+        <?= $this->endSection() ?>
 
-    </div>
-    <!-- ./wrapper -->
-
+   
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <!-- Bootstrap JS -->
