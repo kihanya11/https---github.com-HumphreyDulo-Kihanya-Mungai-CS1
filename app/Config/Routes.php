51aa179536aa59b97458b0f1e41a87880281288a
kihanya11 <kihanya01@gmail.com>
2023-07-13 15:44:02 +0300

    bookings

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -92,6 +92,7 @@ $routes->post('post', 'Post::add_product');
 $routes->get('/admin/products', 'Admin::viewProducts');
 $routes->get('admin/delete_product/(:num)', 'Admin::deleteProduct/$1');
 $routes->get('product/(:num)', 'ProductController::show/$1');
+$routes->post('product/booking/(:num)', 'BookingController::index/$1');
 
 
 
